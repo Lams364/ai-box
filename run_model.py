24afd7e6a9610b6eae4a6c8db6cea21b95ae29d6
Philippe Beaufort <philippe.beaufort.1@ens.etsmtl.ca>
2024-11-01 19:47:31 -0400

    Fix verification du token, add method in extension to change model, change token and get model info

--- run_model.py
+++ run_model.py
@@ -1,6 +1,7 @@
 # app.py
 from flask import Flask, request, jsonify
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import requests
 import torch
 import os
 
@@ -17,9 +18,14 @@ TOKENIZER = None
 DEVICE = None
 MODEL_NAME = None
 
+def validate_huggingface_token(token: str) -> bool:
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get("https://huggingface.co/api/whoami-v2", headers=headers)
+    return response.status_code == 200
+
 def set_model_name(model_name: str):
-    global MODEL, TOKENIZER, DEVICE, MODEL_NAME  # Reference the globals
-    response = create_model(model_name)
+    global MODEL, TOKEN, TOKENIZER, DEVICE, MODEL_NAME  # Reference the globals
+    response = create_model(model_name, TOKEN)
     if response is not None:
         MODEL, TOKENIZER, DEVICE = response
         MODEL_NAME = model_name
@@ -28,31 +34,30 @@ def set_model_name(model_name: str):
         return False
     
 def set_token(new_token:str):
-    global MODEL, TOKEN, TOKENIZER, DEVICE, MODEL_NAME  # Reference the globals
-    old_token = TOKEN
-    TOKEN = new_token
-    response = create_model(MODEL_NAME)
-    if response is not None:
+    global MODEL, TOKEN, TOKENIZER, DEVICE, MODEL_NAME  # Reference the globals   
+    validate = validate_huggingface_token(new_token)
+    response = create_model(MODEL_NAME, new_token)
+    if validate and response is not None:
         MODEL, TOKENIZER, DEVICE = response
+        TOKEN = new_token
         return True
     else:
-        TOKEN = old_token
         return False
 
 
-def create_model(model_name: str):
+def create_model(model_name: str, token: str):
     try:
         # Check if the model is already in the model directory, otherwise download it
         if not os.path.exists(os.path.join(MODEL_DIR, model_name)):
-            tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, cache_dir=MODEL_DIR, token=TOKEN)
+            tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, cache_dir=MODEL_DIR, token=token)
             # Add a new [PAD] token
             tokenizer.add_special_tokens({'pad_token': '[PAD]'})    
-            model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True, cache_dir=MODEL_DIR, token=TOKEN, pad_token_id=tokenizer.eos_token_id)
+            model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True, cache_dir=MODEL_DIR, token=token, pad_token_id=tokenizer.eos_token_id)
         else:
-            tokenizer = AutoTokenizer.from_pretrained(MODEL_DIR, trust_remote_code=True, token=TOKEN)
+            tokenizer = AutoTokenizer.from_pretrained(MODEL_DIR, trust_remote_code=True, token=token)
             # Add a new [PAD] token
             tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-            model = AutoModelForCausalLM.from_pretrained(MODEL_DIR, trust_remote_code=True, token=TOKEN, pad_token_id=tokenizer.eos_token)
+            model = AutoModelForCausalLM.from_pretrained(MODEL_DIR, trust_remote_code=True, token=token, pad_token_id=tokenizer.eos_token)
         
         # Move model to GPU if available
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -96,7 +101,7 @@ def predict():
     outputs = model.generate(**inputs, max_new_tokens=nb_tokens)
     result = tokenizer.decode(outputs[0], skip_special_tokens=True)
     
-    return jsonify({'result': result})
+    return jsonify({'content': result})
 
 @app.route('/change_model')
 def change_model():
@@ -110,11 +115,11 @@ def change_token():
     # Use like this /change_token?token=HUGGINGFACE_TOKEN
     token = request.args.get('token')
     completed = set_token(token)
-    return jsonify({'completed': completed, "token" : TOKEN})
+    return jsonify({'completed': completed})
 
 @app.route('/model_info')
 def model_info():
-    return jsonify({'model': MODEL_NAME, "token" : TOKEN, "device": str(DEVICE)})
+    return jsonify({'model': MODEL_NAME, "device": str(DEVICE)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888)
