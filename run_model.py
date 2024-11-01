303f53bd4ea576c589c3e8c9df9599b6c7e2a3cb
Philippe Beaufort <philippe.beaufort.1@ens.etsmtl.ca>
2024-11-01 18:23:15 -0400

    Add endpoints to change model, change token and get model info

--- run_model.py
+++ run_model.py
@@ -6,32 +6,79 @@ import os
 
 app = Flask(__name__)
 
-# Define the model directory (persistent volume)
 MODEL_DIR = './models'
 
-# Ensure the directory exists
 os.makedirs(MODEL_DIR, exist_ok=True)
 
-# Model details
-model_name = "meta-llama/Llama-3.2-1B-Instruct"
-token = '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'
+# Define globals
+MODEL = None
+TOKEN = None
+TOKENIZER = None
+DEVICE = None
+MODEL_NAME = None
 
-# Check if the model is already in the model directory, otherwise download it
-if not os.path.exists(os.path.join(MODEL_DIR, model_name)):
-    tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=MODEL_DIR, token=token)
-    # Add a new [PAD] token
-    tokenizer.add_special_tokens({'pad_token': '[PAD]'})    
-    model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=MODEL_DIR, token=token, pad_token_id=tokenizer.eos_token_id)
-else:
+def set_model_name(model_name: str):
+    global MODEL, TOKENIZER, DEVICE, MODEL_NAME  # Reference the globals
+    response = create_model(model_name)
+    if response is not None:
+        MODEL, TOKENIZER, DEVICE = response
+        MODEL_NAME = model_name
+        return True
+    else:
+        return False
     
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_DIR, token=token)
-    # Add a new [PAD] token
-    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-    model = AutoModelForCausalLM.from_pretrained(MODEL_DIR, token=token, pad_token_id=tokenizer.eos_token)
+def set_token(new_token:str):
+    global MODEL, TOKEN, TOKENIZER, DEVICE, MODEL_NAME  # Reference the globals
+    old_token = TOKEN
+    TOKEN = new_token
+    response = create_model(MODEL_NAME)
+    if response is not None:
+        MODEL, TOKENIZER, DEVICE = response
+        return True
+    else:
+        TOKEN = old_token
+        return False
 
-# Move model to GPU if available
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to(device)
+
+def create_model(model_name: str):
+    try:
+        # Check if the model is already in the model directory, otherwise download it
+        if not os.path.exists(os.path.join(MODEL_DIR, model_name)):
+            tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, cache_dir=MODEL_DIR, token=TOKEN)
+            # Add a new [PAD] token
+            tokenizer.add_special_tokens({'pad_token': '[PAD]'})    
+            model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True, cache_dir=MODEL_DIR, token=TOKEN, pad_token_id=tokenizer.eos_token_id)
+        else:
+            tokenizer = AutoTokenizer.from_pretrained(MODEL_DIR, trust_remote_code=True, token=TOKEN)
+            # Add a new [PAD] token
+            tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+            model = AutoModelForCausalLM.from_pretrained(MODEL_DIR, trust_remote_code=True, token=TOKEN, pad_token_id=tokenizer.eos_token)
+        
+        # Move model to GPU if available
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model.to(device)
+        return model, tokenizer, device
+    except Exception as e:
+        return None
+    
+def init_model():
+    global MODEL, TOKEN, MODEL_NAME  # Reference the globals
+    # Model details
+    # meta-llama/Llama-3.2-1B-Instruct
+    # Qwen/Qwen2.5-Coder-1.5B-Instruct
+
+    MODEL_NAME = "meta-llama/Llama-3.2-1B-Instruct"
+    TOKEN = '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'
+
+    # TOKEN1 = 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
+    # TOKEN2 = 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
+    set_model_name(MODEL_NAME)
+    print(next(MODEL.parameters()).device)
+
+def test_token(token:str):
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, trust_remote_code=True, cache_dir=MODEL_DIR, token=TOKEN)       
+
+init_model()
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -39,12 +86,36 @@ def predict():
     if 'text' not in data:
         return jsonify({'error': 'No text provided'}), 400
     
-    inputs = tokenizer(data['text'], return_tensors="pt", padding=True, truncation=True).to(device)
+    model = MODEL
+    tokenizer = TOKENIZER
+    text = data['text']
+    nb_tokens = data.get('tokens', 128)
+    
+    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True).to(DEVICE)
     inputs['attention_mask'] = inputs['attention_mask'] if 'attention_mask' in inputs else None
-    outputs = model.generate(**inputs, max_new_tokens=200)
+    outputs = model.generate(**inputs, max_new_tokens=nb_tokens)
     result = tokenizer.decode(outputs[0], skip_special_tokens=True)
     
     return jsonify({'result': result})
 
+@app.route('/change_model')
+def change_model():
+    # Use like this /change_model?name=MODEL_NAME(HUGGINGFACE)
+    name = request.args.get('name')
+    completed = set_model_name(name)
+    return jsonify({'completed': completed, "model_name": MODEL_NAME})
+
+@app.route('/change_token')
+def change_token():
+    # Use like this /change_token?token=HUGGINGFACE_TOKEN
+    token = request.args.get('token')
+    completed = set_token(token)
+    return jsonify({'completed': completed, "token" : TOKEN})
+
+@app.route('/model_info')
+def model_info():
+    return jsonify({'model': MODEL_NAME, "token" : TOKEN, "device": str(DEVICE)})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888)
+
